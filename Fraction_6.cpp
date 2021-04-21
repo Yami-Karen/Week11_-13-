6086e15399c4dddc88be04d831631f09d5f84925
Karen <77882169+Yami-Karen@users.noreply.github.com>
2021-04-21 22:07:50 +0700

    Update Fraction_6.cpp

--- Fraction_6.cpp
+++ Fraction_6.cpp
@@ -1,9 +1,9 @@
 #include "Fraction.h"
 int main()
 {
-	Fraction f(1, 4);
-	Fraction c(3, 4);
-	(f + c).print();
+	Fraction f(1, 2);
+	Fraction c(1, 1);
+	(f + c + f).print();
 	(f - c).print();
 	(f * c).print();
 	(f / c).print();
